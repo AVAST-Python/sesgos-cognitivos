637fd031c169dd86f9408f7f39dd103a6018e9c6
Alvaro Maceda <alvaro@alvaromaceda.es>
2024-03-08 14:06:35 +0100

    Adds wheel

--- src/pages/LossAversion.jsx
+++ src/pages/LossAversion.jsx
@@ -1,7 +1,47 @@
+import { useState } from 'react'
+import { Wheel } from 'react-custom-roulette'
+
+// https://www.npmjs.com/package/react-custom-roulette
+// https://stackoverflow.com/questions/67941474/react-custom-roulette-how-to-show-the-roulette-number
+
+const data = [
+  { option: '0', style: { backgroundColor: 'green', textColor: 'black' } },
+  { option: '1', style: { backgroundColor: 'white' } },
+  { option: '2' },
+]
+
+
 function LossAversion() {
+
+  const [mustSpin, setMustSpin] = useState(false);
+  const [prizeNumber, setPrizeNumber] = useState(0);
+
+  const handleSpinClick = () => {
+    if (!mustSpin) {
+      const newPrizeNumber = Math.floor(Math.random() * data.length)
+      console.log(newPrizeNumber)
+      setPrizeNumber(newPrizeNumber)
+      setMustSpin(true)
+    }
+  }
+
   return (
     <div>
       Loss aversion activity
+
+      <div className="wheel-container">
+        <Wheel
+          mustStartSpinning={mustSpin}
+          prizeNumber={prizeNumber}
+          data={data}
+          spinDuration={0.3}
+          disableInitialAnimation={true}
+          onStopSpinning={() => {
+            setMustSpin(false);
+          }}
+        />
+      </div>
+      <button onClick={handleSpinClick}>SPIN</button>
     </div>
   )
 }

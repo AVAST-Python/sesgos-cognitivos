9f1e4669e4dac8a59443b037e1139a71f2e7b074
Alvaro Maceda <alvaro@alvaromaceda.es>
2024-03-08 11:45:34 +0100

    Anchoring Activity

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ReactSession } from 'react-client-session'
+
 import App from './App.jsx'
 import './index.css'
+
+ReactSession.setStoreType("localStorage")
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/pages/Anchoring.jsx
+++ src/pages/Anchoring.jsx
@@ -1,7 +1,104 @@
-function Anchoring() {
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import './Anchoring.css'
+
+const ANCHOR_LOW = 'low'
+const ANCHOR_HIGH = 'high'
+
+import { ReactSession }  from 'react-client-session'
+
+function Question(props) {
+  const { question, onAnswer } = props
+  const [answer, setAnswer] = useState('')
+
   return (
     <div>
-      Anchoring activity
+      <p>{question}</p>
+      <input type="text" onChange={e => setAnswer(e.target.value)} required />
+      <button onClick={() => onAnswer(answer)}>Responder</button>
+    </div>
+  )
+}
+
+function AnchoringQuestionHigh({onAnswer}) {
+  return (
+    <Question
+      question="¿Cuánto es la mitad de 50.000.000"
+      onAnswer={onAnswer}
+    />
+  )
+}
+
+function AnchoringQuestionLow({onAnswer}) {
+  return (
+    <Question
+      question="¿Cuánto es (5+3)/2-1?"
+      onAnswer={onAnswer}
+    />
+  )
+}
+
+function TestQuestion({onAnswer}) {
+  return (
+    <Question
+      question="¿Qué porcentaje de los países de África pertenecen a la ONU?"
+      onAnswer={onAnswer}
+    />
+  )
+}
+
+function Step1({mode, onAnswer}) {
+  return (
+    <>
+    { mode === ANCHOR_HIGH ?
+        <AnchoringQuestionHigh onAnswer={onAnswer}/> :
+        <AnchoringQuestionLow onAnswer={onAnswer}/>}
+    </>
+  )
+}
+
+function Step2({onAnswer}) {
+  return (
+    <TestQuestion onAnswer={onAnswer}/>
+  )
+}
+
+function Finished() {
+  return (
+    <div>
+      <p>Has terminado el test</p>
+      <Link to="/">Volver al menú</Link>
+    </div>
+  )
+}
+
+function Anchoring() {
+
+  const [step, setStep] = useState(1)
+  const [mode, setMode] = useState(null)
+
+  useEffect(() => {
+    let mode = ReactSession.get("anchoring-mode")
+    if(!mode) {
+      const random = Math.floor(Math.random() * 2)
+      mode = random === 0 ? ANCHOR_LOW : ANCHOR_HIGH
+    }
+
+    setMode(mode)
+    ReactSession.set("anchoring-mode", mode)
+  }, [])
+
+  function nextStep(value) {
+    if (step === 2) ReactSession.set("anchoring-response", value)
+    console.log(value)
+    setStep(step + 1)
+  }
+
+  return (
+    <div>
+      { step === 1 && <Step1 mode={mode} onAnswer={nextStep}/> }
+      { step === 2 && <Step2 onAnswer={nextStep}/> }
+      { step === 3 && <Finished/> }
     </div>
   )
 }
